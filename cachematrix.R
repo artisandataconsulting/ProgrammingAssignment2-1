82441d87353e83065865adda4a0855f0547ce463
jtong <tongconsulting@gmail.com>
2015-06-21 16:45:05 -0400

    Implement the assignment requirement.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R implements Programming Assignment 2 to cache the calculation results.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix will create a "matrix" wrapper "objects" that has
+## access to computed/stored inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    ## whenever set is called, reset the reference to x (the matrix)
+    ## and the inverse if any.
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setInverse <- function(i) {
+        inverse <<- i
+    }
+    getInverse <- function() {
+        inverse
+    }
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Checking to see if inverse has already been computed, if yes,
+##   returned the computed inverse if not, use solve to compute it,
+##   before returning, cache it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if (!is.null(inverse)) {
+        message("returning previously calculated inverse")
+        return (inverse)
+    }
+    matrix <- x$get()
+    inverse <- solve(matrix, ...)
+    x$setInverse(inverse)
+    ## return the calculated result after caching it
+    inverse
 }
